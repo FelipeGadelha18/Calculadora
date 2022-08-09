3c9b13574d18188fdb2394ea30c2a1ac955f7d57
ViniVasc <uderlanio.lima@sou.ufac.br>
2022-08-09 19:57:19 -0300

    #2

--- calculadora.py
+++ calculadora.py
@@ -3,3 +3,12 @@ number_2 = int(input('Segundo Numero'))
 
 print ('{} + {} = '.format (number_1, number_2))
 print (number_1 + number_2)
+
+print ('{} - {} = '.format (number_1, number_2))
+print (number_1 + number_2)
+
+print ('{} * {} = '.format (number_1, number_2))
+print (number_1 + number_2)
+
+print ('{} / {} = '.format (number_1, number_2))
+print (number_1 + number_2)
